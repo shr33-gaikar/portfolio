143e64d89851e1cd94a2b1ef783bc63df747630d
Shrikar Gaikar <139262053+shr33-gaikar@users.noreply.github.com>
2024-05-27 10:17:52 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,7 +13,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/shr33-gaikar",
     resume:
-      "https://drive.google.com/file/d/1XuX5P8qroBEB5mu3PZXhgq9mBmCtGhl5/view?usp=sharing",
+      "https://drive.google.com/file/d/1nSDiyGo7KgJwdFFblRvNMfm6WGpw9VCW/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/shrikar-gaikar/",
     twitter: "",
     insta: "https://www.instagram.com/_shr33.gaikar_/",
